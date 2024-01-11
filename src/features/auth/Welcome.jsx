88b18395004c0fed101140861a2db27c13942a66
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-01-11 14:58:37 +0700

    Add authorization: limit link for non admin

--- src/features/auth/Welcome.jsx
+++ src/features/auth/Welcome.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
+import useAuth from '../../hooks/useAuth';
 
 const Welcome = () => {
+  const { isAdmin, isManager } = useAuth()
 
   const date = new Date()
   const today = new Intl.DateTimeFormat('id-ID', { dateStyle: 'full', timeStyle: 'long' }).format(date);
@@ -15,9 +17,9 @@ const Welcome = () => {
       <p><Link to="/dash/notes">View techNotes</Link></p>
       <p><Link to="/dash/notes/add">Add New Note</Link></p>
 
-      <p><Link to="/dash/users">View User Settings</Link></p>
+      {(isAdmin || isManager) && <p><Link to="/dash/users">View User Settings</Link></p>}
 
-      <p><Link to="/dash/users/add">Add New User</Link></p>
+      {(isAdmin || isManager) && <p><Link to="/dash/users/add">Add New User</Link></p>}
 
     </section>
   )
