f4c2f76a6f86a0ade3918b405856ae57b6899668
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-01-11 10:14:09 +0700

    Add useAuth hook to decode token

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,30 @@
+import { jwtDecode } from "jwt-decode";
+import { useSelector } from "react-redux";
+import { selectCurrentToken } from "../features/auth/authSlice";
+
+const useAuth = () => {
+  const token = useSelector(selectCurrentToken);
+  let isAdmin = false;
+  let isManager = false;
+  let status = 'employee';
+
+  if (token) {
+    // const { username, roles } = jwtDecode(token);
+    const { UserInfo } = jwtDecode(token);
+    const { username, roles } = UserInfo;
+    console.log(UserInfo)
+
+
+    isManager = roles.includes('manager');
+    isAdmin = roles.includes('admin');
+
+    if (isManager) status = 'manager';
+    if (isAdmin) status = 'admin';
+
+    return { username, roles, isAdmin, isManager, status, }
+  }
+
+  return { username: '', roles: [], isAdmin, isManager, status, }
+}
+
+export default useAuth
