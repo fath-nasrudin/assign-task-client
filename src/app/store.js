da97f73c426a2b30e43bcf6a7f44977a03149b98
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-01-13 15:59:18 +0700

    Disable redux devtools when in production

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,8 @@ import { setupListeners } from '@reduxjs/toolkit/query'
 import { apiSlice } from "./api/apiSlice";
 import authSlice from "../features/auth/authSlice";
 
+const devTools = process.env.NODE_ENV !== 'production'
+
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
@@ -10,7 +12,7 @@ export const store = configureStore({
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware().concat(apiSlice.middleware),
-  devTools: true,
+  devTools,
 })
 
 setupListeners(store.dispatch)
