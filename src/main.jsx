d9fdc92deb8cda1fe9e613bb6dcad2b36a95210a
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-01-13 15:58:17 +0700

    Disable react devtools when in production env

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './app/store.js'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
